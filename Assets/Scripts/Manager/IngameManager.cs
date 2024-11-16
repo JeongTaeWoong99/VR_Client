fca443add15be2cf08698728a422df8cde560e46
JeongTaeWoong99 <111197095+JeongTaeWoong99@users.noreply.github.com>
2024-11-16 22:51:07 +0900

    기능 추가
    - CMS가 나의 화면을 보고 있는 경우에만 바이트 전송하도록 수정.
    - 모든 씬, 포톤 패킷 전송 10FPS로 고정.
    - IngameManager 및 FM_System 등등 코드 통합 및 최적화

--- Assets/Scripts/Manager/IngameManager.cs
+++ Assets/Scripts/Manager/IngameManager.cs
@@ -1,20 +1,32 @@
+using System.Collections;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
+using UnityEngine;
+using UnityEngine.UI;
 
 // 미러링 + 화면 공유 공통 매니저
-// 여기다가 ReturnToMainMenu같은 미러링방 + 화면공유방 공통으로 씌일 녀석들을 넣으면 될 듯
+// 여기다가 ReturnToMainMenu같은 공통 UI가 들어감
 public class IngameManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     public static IngameManager instance;
+    
+    [Header("미러링/화면공유 공통")]
+    public FadeCanvas fadeCanvas;
+    public float      fadeDuration = 1.0f;
+    
+    public Button     mainMenuButton;           // 메인메뉴로 돌아가기 버튼
+    
+    [Header("미러링")]
+    public GameViewEncoder _gameViewEncoder;    // 미러링 전용
 
     private void Awake()
     {
         instance = this;
         
-        PhotonNetwork.SendRate          = 30; // 초당 서버로 보내는 패킷 횟수 (기본값 20)
-        PhotonNetwork.SerializationRate = 30; // 초당 동기화되는 데이터 횟수 (기본값 10)
+        PhotonNetwork.SendRate          = 10; // 초당 서버로 보내는 패킷 횟수 (기본값 20)
+        PhotonNetwork.SerializationRate = 10; // 초당 동기화되는 데이터 횟수 (기본값 10)
     }
     
     void Start()
@@ -42,13 +54,49 @@ public class IngameManager : MonoBehaviourPunCallbacks, IOnEventCallback
     {
         PhotonNetwork.RemoveCallbackTarget(this);
     }
-
     
-    // Room을 나갈 때, 호출
-    // UIController의 ReturnToMainMenu()를 사용하기 위해서, 필요
-    public override void OnLeftRoom()
+    // 방에 입장 완료시 호출(CreateRoom하고 난 후 or 만들어져 있는 방을 룸버튼 클릭하여, 들어가면)
+    public override void OnJoinedRoom()
     {
-        base.OnLeftRoom();
-        SceneManager.LoadScene(0);
+        Debug.Log("방입장 성공");
+        if(_gameViewEncoder)    // 미러링에서 CreateRoom or JoinRoom으로 성공 시, 라벨 번호 세팅
+            _gameViewEncoder.label = PhotonNetwork.LocalPlayer.ActorNumber; // 엑터 넘버를 라벨 번호로 설정.
+    }
+    
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        // 이미 교육용 게임 방이 존재하는 경우
+        if (returnCode == 32766)
+        {
+            Debug.Log("방 이미 존재 -> JoinRoom");
+            PhotonNetwork.JoinRoom(PlayerPrefs.GetString("roomName")); // Join
+        }
+        // 그 외, 오류 표시
+        else
+        {
+            Debug.Log(returnCode);
+        }
+    }
+    
+    [PunRPC] // 버튼으로 사용 + CMS RPC로도 사용
+    public void OnReturnToMainMenu()
+    {
+        StartCoroutine(ReturnToMainMenu());
+    }
+    
+    private IEnumerator ReturnToMainMenu()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            mainMenuButton.interactable = false; // 버튼 비활성화(중복 누르기 방지)
+            
+            PhotonNetwork.AutomaticallySyncScene = false;
+            PhotonNetwork.Disconnect();                     // 서버와 연결 끊기
+            
+            fadeCanvas.QuickFadeIn();                       // 페이드인                
+            yield return new WaitForSeconds(fadeDuration);  
+            
+            SceneManager.LoadScene("Main Menu"); // Main Menu Test에서 Main Menu으로 돌아가면, 오류.(XR 중복)
+        }
     }
 }
--- Assets/Scripts/Manager/SharedRoomManager.cs
+++ Assets/Scripts/Manager/SharedRoomManager.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using Photon.Pun;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using UnityEngine.Video;
 
 // 화면 공유 전용
@@ -11,13 +8,9 @@ public class SharedRoomManager : MonoBehaviourPunCallbacks
 {
     public static SharedRoomManager instance;
 
-    public Button mainMenuButton; // 메인메뉴로 돌아가기 버튼
-
     public GameObject[] objectsToHide;
-    public FadeCanvas   fadeCanvas;
     public Material     videoMaterial;
     public VideoPlayer  videoPlayer;
-    public float        fadeDuration = 1.0f;
 
     private Material _skyMaterial;
 
@@ -25,8 +18,8 @@ public class SharedRoomManager : MonoBehaviourPunCallbacks
     {
         instance = this;
         
-        PhotonNetwork.SendRate          = 30; // 초당 서버로 보내는 패킷 횟수 (기본값 20)
-        PhotonNetwork.SerializationRate = 30; // 초당 동기화되는 데이터 횟수 (기본값 10)
+        PhotonNetwork.SendRate          = 10; // 초당 서버로 보내는 패킷 횟수 (기본값 20)
+        PhotonNetwork.SerializationRate = 10; // 초당 동기화되는 데이터 횟수 (기본값 10)
     }
 
     private void Start()
@@ -53,12 +46,6 @@ public class SharedRoomManager : MonoBehaviourPunCallbacks
         RenderSettings.skybox = targetMaterial;
         onCompleteAction.Invoke();
     }
-
-    [PunRPC] // 버튼으로 사용 + CMS RPC로도 사용
-    public void OnReturnToMainMenu()
-    {
-        StartCoroutine(ReturnToMainMenu());
-    }
     
     [PunRPC]    // 슬라이더 이동 동기화
     public void VideoTimeChange(double newTime)
@@ -70,24 +57,8 @@ public class SharedRoomManager : MonoBehaviourPunCallbacks
     public void VideoSetting(bool isPlaying, double currentTime)
     {
         // 세팅 넣어주기(경로 / 시간 / 재생상태 등등)
-        videoPlayer.url = PlayerPrefs.GetString("videoPath"); // 동영상 위치를 정적으로 받아서, 바로 넣어주기
-        videoPlayer.time = currentTime;                          // 재생 시간 동기화
-        if (isPlaying) StartVideo();                             // 재생 상태 동기화
-    }
-
-    private IEnumerator ReturnToMainMenu()
-    {
-        if (PhotonNetwork.InRoom)
-        {
-            mainMenuButton.interactable = false; // 버튼 비활성화(중복 누르기 방지)
-            
-            PhotonNetwork.AutomaticallySyncScene = false;
-            PhotonNetwork.Disconnect();                     // 서버와 연결 끊기
-            
-            fadeCanvas.QuickFadeIn();                       // 페이드인                
-            yield return new WaitForSeconds(fadeDuration);  
-            
-            SceneManager.LoadScene("Main Menu"); // Main Menu Test에서 Main Menu으로 돌아가면, 오류.(XR 중복)
-        }
+        videoPlayer.url  = PlayerPrefs.GetString("videoPath"); // 동영상 위치를 정적으로 받아서, 바로 넣어주기
+        videoPlayer.time = currentTime;                           // 재생 시간 동기화
+        if (isPlaying) StartVideo();                              // 재생 상태 동기화
     }
 }
