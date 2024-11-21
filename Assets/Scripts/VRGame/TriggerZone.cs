137f5173e62aa6a34a71d3bf39b0dea3eaba1ad9
dlwnsdudvv <songjw4654@gmail.com>
2024-11-21 17:04:47 +0900

    수정

--- Assets/Scripts/VRGame/TriggerZone.cs
+++ Assets/Scripts/VRGame/TriggerZone.cs
@@ -27,7 +27,7 @@ public class TriggerZone : MonoBehaviour
             
             AudioManager.instance.Play("Ending");
             
-            IngameManager.instance.OnReturnToMainMenu(5f);
+            StartCoroutine(IngameManager.instance.ReturnToMainMenu(5f));
         }
     }
 
